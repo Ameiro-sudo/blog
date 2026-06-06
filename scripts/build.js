53393f26f5308e3c3ee50b0f4dcbaf2e3843bd80
ninasukiwww-png <ninasukiwww@gmail.com>
2026-06-06 10:44:34 +0800

    恢复图床 CDN 引用，删除本地图片

--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,7 @@ const ROOT = join(__dirname, '..')
 
 const POSTS_DIR = join(ROOT, 'content', 'posts')
 const ALBUMS_DIR = join(ROOT, 'content', 'albums')
-const CDN_BASE = 'assets/img'
+const CDN_BASE = 'https://cdn.jsdelivr.net/gh/ninasukiwww-png/my-images/blog'
 
 function parseFrontmatter(text) {
   const lines = text.split('\n')
