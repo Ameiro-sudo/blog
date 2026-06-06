90cc8c2e872d506ea75374e667c3ef9e2a2dc811
ninasukiwww-png <ninasukiwww@gmail.com>
2026-06-06 10:55:11 +0800

    归档置顶文章单独列出

--- assets/js/app.js
+++ assets/js/app.js
@@ -414,7 +414,9 @@
     // === 归档 ===
     function renderArchive() {
       var groups = {}
+      var pinnedPosts = []
       postsMeta.forEach(function (p) {
+        if (p.pinned) { pinnedPosts.push(p); return }
         var year = p.date ? p.date.substring(0, 4) : '未知'
         if (!groups[year]) groups[year] = {}
         var month = p.date ? p.date.substring(5, 7) : '??'
@@ -429,8 +431,17 @@
         if (!groups[year][month]) groups[year][month] = { posts: [], albums: [] }
         groups[year][month].albums.push(a)
       })
-      var years = Object.keys(groups).sort().reverse()
       var html = ''
+      if (pinnedPosts.length) {
+        html += '<div class="archive-year"><div class="archive-year-header">&#x1F4CC; 置顶</div><div class="archive-month"><ul class="archive-list">'
+        pinnedPosts.forEach(function (p) {
+          html += '<li class="archive-item" data-id="' + p.id + '">' +
+            '<span class="archive-item-date">' + (p.date ? p.date.substring(8, 10) : '') + '</span>' +
+            '<span class="archive-item-title">' + p.title + '</span></li>'
+        })
+        html += '</ul></div></div>'
+      }
+      var years = Object.keys(groups).sort().reverse()
       years.forEach(function (year) {
         html += '<div class="archive-year"><div class="archive-year-header">' + year + '</div>'
         var months = Object.keys(groups[year]).sort().reverse()
